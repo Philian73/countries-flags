29439591387a3bf607eb5c6b330fa74906c085a1
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-09-18 20:59:39 +0300

    updated

--- src/features/controls/controls.tsx
+++ src/features/controls/controls.tsx
@@ -1,13 +1,37 @@
 import { useState } from 'react'
 
-import { Search } from './'
+import { Wrapper } from './controls.styled'
+
+import { Search, CustomSelect } from './'
+
+const options: OptionType[] = [
+  { value: 'Africa', label: 'Africa' },
+  { value: 'America', label: 'America' },
+  { value: 'Asia', label: 'Asia' },
+  { value: 'Europe', label: 'Europe' },
+  { value: 'Oceania', label: 'Oceania' },
+]
 
 export const Controls = ({}) => {
   const [search, setSearch] = useState('')
+  const [region, setRegion] = useState<unknown>('')
 
   return (
-    <div>
+    <Wrapper>
       <Search search={search} setSearch={setSearch} />
-    </div>
+      <CustomSelect
+        options={options}
+        placeholder="Filter by Region"
+        isClearable
+        isSearchable={false}
+        value={region}
+        onChange={setRegion}
+      />
+    </Wrapper>
   )
+}
+
+type OptionType = {
+  value: string
+  label: string
 }
