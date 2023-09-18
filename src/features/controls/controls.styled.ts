a6a8548186f86b09ff2f22260c46169c44ecb7a4
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-09-18 20:59:27 +0300

    styled for controls

---
+++ src/features/controls/controls.styled.ts
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components'
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+
+  @media (min-width: 767px) {
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
+`
