ab9762f3874b91c15c903c767ff8fe1f205d5d4a
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-09-21 19:46:05 +0300

    wrapped BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 import '@/app/styles/index.scss'
 
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 
 import { App } from '@/app/App'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
