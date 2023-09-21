08a7b21655e6109a7c3330eebdcf19bbab9d872f
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-09-21 20:20:23 +0300

    props HomePage

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,8 @@
+import { useState } from 'react'
+
 import { Navigate, Route, Routes } from 'react-router-dom'
 
+import { CountryType } from '@/features/card/model'
 import { Details } from '@/pages/details'
 import { HomePage } from '@/pages/home'
 import { NotFound } from '@/pages/not-found'
@@ -7,12 +10,17 @@ import { Header } from '@/widgets/header'
 import { Main } from '@/widgets/main'
 
 export const App = () => {
+  const [countries, setCountries] = useState<CountryType[]>([])
+
   return (
     <>
       <Header />
       <Main>
         <Routes>
-          <Route path="/" element={<HomePage />} />
+          <Route
+            path="/"
+            element={<HomePage countries={countries} setCountries={setCountries} />}
+          />
           <Route path="/country/:name" element={<Details />} />
 
           <Route path="/404" element={<NotFound />} />
