a7f3260fd8aca4d6fcf066a3beab971200e6437b
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-09-21 20:15:46 +0300

    temp

--- src/pages/details/details.tsx
+++ src/pages/details/details.tsx
@@ -1,6 +1,10 @@
 import { FC } from 'react'
 
+import { useParams } from 'react-router-dom'
+
 type PropsType = {}
 export const Details: FC<PropsType> = ({}) => {
-  return <div></div>
+  const params = useParams()
+
+  return <div>Details {params.name}</div>
 }
