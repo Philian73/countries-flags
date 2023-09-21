0c1150fac3eec7bda04f5243c3d0ba964d9c2f1f
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-09-21 23:06:31 +0300

    styles for Info.tsx

---
+++ src/features/card/ui/info/info.styled.ts
@@ -0,0 +1,13 @@
+import { styled } from 'styled-components'
+
+export const Wrapper = styled.section``
+
+export const InfoImage = styled.img``
+
+export const InfoTitle = styled.h1``
+
+export const ListGroup = styled.div``
+
+export const List = styled.ul``
+
+export const ListItem = styled.li``
