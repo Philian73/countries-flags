e18e4f1802c1bcebc856ceb1175bc350d088d0eb
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-09-21 23:06:41 +0300

    updated

--- src/pages/details/details.tsx
+++ src/pages/details/details.tsx
@@ -1,10 +1,33 @@
-import { FC } from 'react'
+import { FC, useEffect, useState } from 'react'
 
-import { useParams } from 'react-router-dom'
+import { IoArrowBack } from 'react-icons/io5'
+import { useNavigate, useParams } from 'react-router-dom'
+
+import { CountryType } from '@/features/card/model'
+import { Info } from '@/features/card/ui'
+import { baseInstance } from '@/shared/api'
+import { Button } from '@/shared/ui'
 
 type PropsType = {}
 export const Details: FC<PropsType> = ({}) => {
-  const params = useParams()
+  const { name } = useParams()
 
-  return <div>Details {params.name}</div>
+  const navigate = useNavigate()
+
+  const [country, setCountry] = useState<CountryType | null>(null)
+
+  useEffect(() => {
+    baseInstance.get(`name/${name}`).then(({ data }) => setCountry(data[0]))
+  }, [name])
+
+  const handleGoBack = () => navigate(-1)
+
+  return (
+    <div>
+      <Button onClick={handleGoBack}>
+        <IoArrowBack /> Back
+      </Button>
+      {country && <Info country={country} />}
+    </div>
+  )
 }
