322e865039ac5d9a5d08bfdbb9a5f4bb0038fafc
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-09-22 04:42:33 +0300

    used 'ThemeProvider'

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,12 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
 import { App } from '@/app/App'
+import { ThemeProvider } from '@/app/providers'
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <ThemeProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>
 )
