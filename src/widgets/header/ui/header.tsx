726c7cfe71a80d0c9c6ff74ea7855c590f43d171
Nikolay Sulteev (Philian73) <Philian73@gmail.com>
2023-09-22 04:42:16 +0300

    refactor (import useTheme hook)

--- src/widgets/header/ui/header.tsx
+++ src/widgets/header/ui/header.tsx
@@ -1,20 +1,15 @@
-import { FC, useLayoutEffect, useState } from 'react'
+import { FC } from 'react'
 
 import { IoMoon, IoMoonOutline } from 'react-icons/io5'
 
 import { Title, Wrapper, ModeSwitcher, StyledHeader } from './header.styled'
 
+import { useTheme } from '@/app/providers'
 import { Container } from '@/shared/ui'
 
 type PropsType = {}
 export const Header: FC<PropsType> = ({}) => {
-  const [theme, setTheme] = useState<'light' | 'dark'>('light')
-
-  const toggleTheme = () => setTheme(theme === 'light' ? 'dark' : 'light')
-
-  useLayoutEffect(() => {
-    document.body.setAttribute('data-theme', theme)
-  }, [theme])
+  const { theme, toggleTheme } = useTheme()
 
   return (
     <StyledHeader>
